fd3f0fa779e9851c9695293d694889cc4f83b22d
Prelang Builder <builder@prelang.com>
2014-07-04 05:46:02 +0000

    Added #vote action to set model before_action in the votable model's controller

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -3,7 +3,7 @@ class PinsController < ApplicationController
   #->Prelang (scaffolding:rails/scope_to_user)
   before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
 
-  before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :set_pin, only: [:show, :edit, :update, :destroy, :vote]
 
   # GET /pins
   # GET /pins.json
